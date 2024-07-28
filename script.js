43e7abe6d6401def7d6564a17a127cca388b0182
subhajit dhar <subhajit13dhar@gmail.com>
2024-07-28 10:27:31 +0530

    All pages are made

--- script.js
+++ script.js
@@ -17,35 +17,21 @@ setInterval(updateTime, 1000);
 updateTime();
 
 
-//for the no button
-const noButton = document.getElementById('noLast');
-const buttonContainer = document.querySelector('.button-container');
 
-function moveButton() {
-    const containerRect = buttonContainer.getBoundingClientRect();
-    const buttonRect = noButton.getBoundingClientRect();
-    const maxX = containerRect.width - buttonRect.width;
-    const maxY = containerRect.height - buttonRect.height;
 
-    let newX, newY;
-
-    do {
-        newX = Math.random() * maxX *2;
-        newY = Math.random() * maxY *2;
-    } while (Math.abs(newX - buttonRect.left) < 50 && Math.abs(newY - buttonRect.top) < 50);
-
-    noButton.style.left = `${newX}px`;
-    noButton.style.top = `${newY}px`;
-}
-
-noButton.addEventListener('mouseover', moveButton);
-noButton.addEventListener('touchstart', moveButton);
-
-const yesButton = document.getElementById('yes');
-yesButton.addEventListener('click', () => {
-    window.location.href = '/html/last.html';
-});
 
 function yes(){
+    window.location.href = '/html/yes.html';
+}
+
+function page2(){
+    window.location.href = '/html/page2.html';
+}
+
+function page3(){
+    window.location.href = '/html/page3.html';
+}
+
+function last(){
     window.location.href = '/html/last.html';
 }
